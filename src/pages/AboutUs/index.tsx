bc4995008969497448bab1ac4fa110f34cb141f7
Henrique Lopes <henriquebonfanti2018@gmail.com>
2022-11-23 11:24:04 -0300

    Update the About us page.

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -1,9 +1,48 @@
-import { HomeMainContainer } from './styles'
+import {
+  AboutUsMainContainer,
+  AboutUsMainContent,
+  AboutUsValues,
+  CompanyValues,
+} from './styles'
 
 export function AboutUs() {
   return (
-    <HomeMainContainer>
-      <h1>Sobre nós</h1>
-    </HomeMainContainer>
+    <AboutUsMainContainer>
+      <AboutUsMainContent>
+        <h1>Sobre nós</h1>
+        <p>
+          Somos uma empresa apaixonada em transformar o mundo através da
+          tecnologia e a inovação. A NewGO Tecnologia surgiu em 2019 com o
+          objetivo de trazer o melhor para os nossos clientes e pessoas
+          envolvidas, tratando cada cliente como único e entregando uma
+          experiência incrível. Além de impactar com nossa excelência e serviços
+          de TIC. Atualmente a NewGo presta serviços de criação de software,
+          terceirização, treinamento e consultoria.
+        </p>
+      </AboutUsMainContent>
+      <AboutUsValues>
+        <CompanyValues>
+          <h2>MISSÃO</h2>
+          <p>
+            Ser um time obstinado em prever e atender as necessidades de nossos
+            clientes e consumidores sempre agregando valores.
+          </p>
+        </CompanyValues>
+        <CompanyValues>
+          <h2>VISÃO</h2>
+          <p>
+            Disponibilizar às pessoas software de excelente qualidade – a
+            qualquer momento, em qualquer local e em qualquer dispositivo.
+          </p>
+        </CompanyValues>
+        <CompanyValues>
+          <h2>VALORES</h2>
+          <p>
+            Apaixonados; autênticos; comprometidos; honestos; inovadores e
+            inspiradores.
+          </p>
+        </CompanyValues>
+      </AboutUsValues>
+    </AboutUsMainContainer>
   )
 }
--- src/pages/AboutUs/styles.ts
+++ src/pages/AboutUs/styles.ts
@@ -1,10 +1,52 @@
 import styled from 'styled-components'
 
-export const HomeMainContainer = styled.main`
+export const AboutUsMainContainer = styled.main`
   height: 33.8rem;
+  padding: 2rem 1.25rem;
+  gap: 8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
-  span {
-    color: #002abf;
-    font-weight: 900;
+export const AboutUsMainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+
+  h1 {
+    margin-top: 2rem;
+  }
+
+  p {
+    text-align: center;
+    line-height: 1.3;
+    color: ${(props) => props.theme['gray-text']};
+    font-size: 1.125rem;
+  }
+`
+
+export const AboutUsValues = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10rem;
+`
+export const CompanyValues = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 1rem;
+  width: 16rem;
+  height: 16rem;
+
+  h2 {
+    font-size: 1.25rem;
+    font-weight: 500;
+  }
+
+  p {
+    line-height: 1.5;
+    color: ${(props) => props.theme['gray-text']};
   }
 `
