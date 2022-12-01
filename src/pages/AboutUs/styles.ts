6c66fc699b6fa3cfb8c475a5642fc33a19b6acc6
Henrique Lopes <henriquebonfanti2018@gmail.com>
2022-12-01 10:28:44 -0300

    Update the About us page and add responsive design.

--- src/pages/AboutUs/styles.ts
+++ src/pages/AboutUs/styles.ts
@@ -7,6 +7,17 @@ export const AboutUsMainContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (max-width: 872px) {
+    gap: 2rem;
+  }
+
+  @media (max-width: 584px) {
+    height: 45rem;
+  }
+  @media (max-width: 378px) {
+    height: 50rem;
+  }
 `
 
 export const AboutUsMainContent = styled.div`
@@ -25,28 +36,56 @@ export const AboutUsMainContent = styled.div`
     color: ${(props) => props.theme['gray-text']};
     font-size: 1.125rem;
   }
+
+  @media (max-width: 875px) {
+    h1 {
+      margin-top: 1rem;
+      font-size: 1.375rem;
+    }
+    p {
+      font-size: 90%;
+    }
+  }
 `
 
 export const AboutUsValues = styled.div`
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
   gap: 10rem;
+
+  @media (max-width: 1128px) {
+    gap: 5rem;
+  }
+
+  @media (max-width: 970px) {
+    gap: 2rem;
+  }
 `
 export const CompanyValues = styled.div`
   display: flex;
   flex-direction: column;
-  text-align: center;
   gap: 1rem;
   width: 16rem;
-  height: 16rem;
 
   h2 {
     font-size: 1.25rem;
+    text-align: center;
     font-weight: 500;
   }
 
   p {
     line-height: 1.5;
+    text-align: justify;
     color: ${(props) => props.theme['gray-text']};
+  }
+
+  @media (max-width: 875px) {
+    h2 {
+      font-size: 95%;
+    }
+    p {
+      font-size: 90%;
+    }
   }
 `
